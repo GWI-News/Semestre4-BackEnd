075e1910f4c855074ac797cd5cb2ee10f8cd0188
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-10-02 20:05:56 -0300

    Entities Notícias

--- GwiNews/GwiNews.Domain/Entities/News.cs
+++ GwiNews/GwiNews.Domain/Entities/News.cs
@@ -45,11 +45,39 @@ namespace GwiNews.Domain.Entities
 
         //Construtores
 
-        public News() { }
+        public News()
+        {
+        }
 
         public News(Guid id, NewsStatus status, string newsUrl, string title, string subtitle, string newsBody, string imageUrl, DateTime publicationDate/*, UserWithNews author, UserWithNews editor, NewsCategory newsCategory*/)
         {
             Id = id;
+            Status = status;
+            NewsUrl = newsUrl;
+            Title = title;
+            Subtitle = subtitle;
+            NewsBody = newsBody;
+            ImageUrl = imageUrl;
+            PublicationDate = publicationDate;
+            //Author = author;
+            //Editor = editor;
+            //this.newsCategory = newsCategory;
+        }
+
+        //Validações
+
+        public void ValidateDomain(NewsStatus status, string newsUrl, string title, string subtitle, string newsBody, string imageUrl, DateTime publicationDate/*, UserWithNews author, UserWithNews editor, NewsCategory newsCategory*/)
+        {
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(newsUrl) || newsUrl.Length > 255, "A URL da notícia é obrigatória e não pode exceder 255 caracteres.");
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(title) || title.Length > 75, "O título da notícia é obrigatório e não pode exceder 75 caracteres.");
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(subtitle) || subtitle.Length > 255, "O subtítulo da notícia é obrigatório e não pode exceder 255 caracteres.");
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(newsBody) || newsBody.Length > 65.535, "O corpo da notícia é obrigatório e não pode exceder 65.535 caracteres.");
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(imageUrl) || imageUrl.Length > 555, "A URL da imagem é obrigatória e não pode exceder 555 caracteres.");
+            DomainExceptionValidation.When(publicationDate == null, "A data de publicação é obrigatória.");
+            //DomainExceptionValidation.When(author == null, "O autor é obrigatório.");
+            //DomainExceptionValidation.When(editor == null, "O editor é obrigatório.");
+            //DomainExceptionValidation.When(newsCategory == null, "A categoria da notícia é obrigatória.");
+
             Status = status;
             NewsUrl = newsUrl;
             Title = title;
