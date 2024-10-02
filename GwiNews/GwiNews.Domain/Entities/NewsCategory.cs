2f613102a8687921d1e7181457e54ca804fcd21f
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-10-02 19:44:39 -0300

    Implementar a Classe NewsCategory no Diretório Entities

---
+++ GwiNews/GwiNews.Domain/Entities/NewsCategory.cs
@@ -0,0 +1,30 @@
+﻿namespace GwiNews.Domain.Entities
+{
+    public class NewsCategory
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length > 25)
+                {
+                    throw new ArgumentException("O nome da categoria não pode exceder 25 caracteres.");
+                }
+                _name = value;
+            }
+        }
+
+        public ICollection<News> News { get; set; } = new List<News>();
+
+        //public ICollection<NewsSubcategory> NewsSubcategories { get; set; } = new List<NewsSubcategory>();
+
+        public NewsCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
