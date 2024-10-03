e3cc9f51fad94b8db606b80e246d4cc76324134c
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-10-02 22:06:38 -0300

    Implementar a Interface INewsCategoryRepository no Diretório Interfaces

---
+++ GwiNews/GwiNews.Domain/Interfaces/INewsCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using GwiNews.Domain.Entities;
+
+namespace Interfaces
+{
+    public interface INewsCategoryRepository
+    {
+        Task<NewsCategory> GetNewsCategoryById(Guid id);
+        Task<IEnumerable<NewsCategory>> GetAllNewsCategories();
+        Task AddNewsCategory(NewsCategory newsCategory);
+        Task UpdateNewsCategory(NewsCategory newsCategory);
+        Task DeleteNewsCategory(Guid id);
+        Task<IEnumerable<NewsCategory>> GetNewsCategoriesWithSubcategories();
+    }
+}
