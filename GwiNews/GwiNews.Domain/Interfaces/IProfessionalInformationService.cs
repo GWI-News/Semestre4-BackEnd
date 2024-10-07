fd95b7e3791e1f174a891d2cb55215d903b0800d
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 18:51:06 -0300

    Criar a Interface IProfessionalInformationService no diretorio Interfaces

---
+++ GwiNews/GwiNews.Domain/Interfaces/IProfessionalInformationService.cs
@@ -0,0 +1,12 @@
+﻿using gwiBack.Domain.Entities;
+
+namespace gwiBack.Domain.Interfaces
+{
+    public interface IProfessionalInformationService
+    {
+        Task<ProfessionalInformation> GetProfessionalInformationByIdAsync(Guid id);
+        Task<ProfessionalInformation> CreateProfessionalInformationAsync(ProfessionalInformation information);
+        Task<ProfessionalInformation> UpdateProfessionalInformationAsync(ProfessionalInformation information);
+        Task DeleteProfessionalInformationAsync(Guid id);
+    }
+}
