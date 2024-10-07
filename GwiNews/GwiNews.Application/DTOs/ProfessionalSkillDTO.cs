e9857a680763f9864f76d8c785066978796ab9f3
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 19:27:43 -0300

    criar a classe ProfessionalSkillDTO no Diretorio Application.DTO

---
+++ GwiNews/GwiNews.Application/DTOs/ProfessionalSkillDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace gwiBack.Application.DTOs
+{
+    public class ProfessionalSkillDTO
+    {
+        public Guid Id { get; set; }
+        public string Skill1 { get; set; }
+        public string Skill2 { get; set; }
+        public string Skill3 { get; set; }
+        public string Skill4 { get; set; }
+    }
+}
