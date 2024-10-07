2c64592ad1f94a9b31cd47740bf1fb9b795a3e75
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 18:44:29 -0300

    Criar a classe ProfessionalInformationDTO no diretorio Application.DTO

---
+++ GwiNews/GwiNews.Application/DTOs/ProfessionalInformationDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace gwiBack.Application.DTOs
+{
+    public class ProfessionalInformationDTO
+    {
+        public Guid Id { get; set; }
+        public string CompleteName { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Linkedin { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string Objetivo { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
