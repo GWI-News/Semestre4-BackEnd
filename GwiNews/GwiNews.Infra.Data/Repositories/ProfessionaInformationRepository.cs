c8c13da8e76d66b712a2ff416091d4e80ae62053
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 18:10:43 -0300

    Criar a classe ProfessionalInformationRepository no diretorio Repositories, na camada Infra.Data

---
+++ GwiNews/GwiNews.Infra.Data/Repositories/ProfessionaInformationRepository.cs
@@ -0,0 +1,51 @@
+﻿using gwiBack.Domain.Entities;
+using gwiBack.Domain.Interfaces;
+using gwiBack.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace gwiBack.Infra.Data.Repositories
+{
+    public class ProfessionalInformationRepository : IProfessionalInformationRepository
+    {
+        private readonly gwiDbContext _context;
+
+        public ProfessionalInformationRepository(gwiDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<ProfessionalInformation> CreateAsync(ProfessionalInformation information)
+        {
+            await _context.ProfessionalInformation.AddAsync(information);
+            await _context.SaveChangesAsync();
+            return information;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var information = await GetByIdAsync(id);
+            if (information != null)
+            {
+                _context.ProfessionalInformation.Remove(information);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<ProfessionalInformation>> GetAllAsync()
+        {
+            return await _context.ProfessionalInformation.ToListAsync();
+        }
+
+        public async Task<ProfessionalInformation> GetByIdAsync(Guid id)
+        {
+            return await _context.ProfessionalInformation.FindAsync(id);
+        }
+
+        public async Task<ProfessionalInformation> UpdateAsync(ProfessionalInformation information)
+        {
+            _context.ProfessionalInformation.Update(information);
+            await _context.SaveChangesAsync();
+            return information;
+        }
+    }
+}
