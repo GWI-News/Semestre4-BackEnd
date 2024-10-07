ad9279b3de9ca2939d6431a5b60cc5e4cb9405bc
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 18:29:56 -0300

    Criar a interface IFormationRepository no diretorio Interfaces

---
+++ GwiNews/GwiNews.Domain/Interfaces/IFormationRepository.cs
@@ -0,0 +1,18 @@
+﻿using gwiBack.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gwiBack.Domain.Interfaces
+{
+    public interface IFormationRepository
+    {
+        Task<IEnumerable<Formation>> GetAllAsync();
+        Task<Formation?> GetByIdAsync(Guid id);
+        Task<Formation> CreateAsync(Formation formation);
+        Task<Formation> UpdateAsync(Formation formation);
+        Task DeleteAsync(Guid id);
+    }
+}
