ce312547d60e29c97c14281b3f32001f8b7ed4e1
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 18:05:09 -0300

    Criar Interface IProfessionalInformationRepository no diretorio Interfaces

---
+++ GwiNews/GwiNews.Domain/Interfaces/IProfessionalInformationRepository.cs
@@ -0,0 +1,17 @@
+﻿using gwiBack.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gwiBack.Domain.Interfaces
+{
+    public interface IProfessionalInformationRepository
+    {
+        Task<ProfessionalInformation> GetByIdAsync(Guid id);
+        Task<ProfessionalInformation> CreateAsync(ProfessionalInformation information);
+        Task<ProfessionalInformation> UpdateAsync(ProfessionalInformation information);
+        Task DeleteAsync(Guid id);
+    }
+}
