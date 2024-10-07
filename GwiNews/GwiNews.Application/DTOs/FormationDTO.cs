4a09d9ac98eb7caa47855b27ed21f29d5628bc5a
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-07 15:45:08 -0300

    Adicionando a classe FormationDTO no GWINews.Application

---
+++ GwiNews/GwiNews.Application/DTOs/FormationDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace gwiBack.Application.DTOs
+{
+    public class FormationDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Institution { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Activity1 { get; set; }
+        public string Activity2 { get; set; }
+        public string Activity3 { get; set; }
+    }
+}
