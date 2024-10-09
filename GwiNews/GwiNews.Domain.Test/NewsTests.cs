10b7f36fb03e34e20b139a26b0ac1cd59e0c1bcf
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-10-09 11:16:26 -0300

    NewsTests

--- GwiNews/GwiNews.Domain.Test/NewsTests.cs
+++ GwiNews/GwiNews.Domain.Test/NewsTests.cs
@@ -7,163 +7,47 @@ namespace GwiNews.Domain.Test
 {
     public class NewsTests
     {
-        [Fact]
-        public void Constructor_WithValidParameters_ShouldCreateNews()
-        {
-            var news = new News(
-                Guid.NewGuid(),
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "Título da Notícia",
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-            //new NewsCategory()
-            );
 
-            Assert.NotNull(news);
-            Assert.Equal(NewsStatus.Publicada, news.Status);
-            Assert.Equal("Título da Notícia", news.Title);
-            Assert.Equal("Subtítulo da Notícia", news.Subtitle);
-            Assert.Equal("Corpo da notícia.", news.NewsBody);
-            Assert.Equal("https://example.com/news", news.NewsUrl);
-            Assert.Equal("https://example.com/image.jpg", news.ImageUrl);
+        [Fact]
+        public void Should_Throw_Exception_When_Title_Is_Null()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new News(null, "Conteúdo de Teste"));
+            Assert.Equal("O título da notícia não pode ser vazio.", exception.Message);
         }
 
         [Fact]
-        public void ValidateDomain_WithValidParameters_ShouldSetProperties()
-        {
-            var news = new News();
-            news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "Título da Notícia",
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //new NewsCategory()
-            );
 
-            Assert.Equal(NewsStatus.Publicada, news.Status);
-            Assert.Equal("Título da Notícia", news.Title);
-            Assert.Equal("Subtítulo da Notícia", news.Subtitle);
-            Assert.Equal("Corpo da notícia.", news.NewsBody);
-            Assert.Equal("https://example.com/news", news.NewsUrl);
-            Assert.Equal("https://example.com/image.jpg", news.ImageUrl);
+        public void Should_Throw_Exception_When_Title_Is_Empty()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new News("", "Conteúdo de Teste"));
+            Assert.Equal("O título da notícia não pode ser vazio.", exception.Message);
         }
 
         [Fact]
-        public void ValidateDomain_WithEmptyNewsUrl_ShouldThrowDomainException()
+
+        public void Should_Throw_Exception_When_Title_Is_Too_Long()
         {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "",
-                "Título da Notícia",
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //new NewsCategory()
-            ));
+            var longTitle = new string('A', 76);
+
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new News(longTitle, "Conteúdo de Teste"));
+            Assert.Equal("O título da notícia não pode exceder 75 caracteres.", exception.Message);
         }
 
         [Fact]
-        public void ValidateDomain_WithLongTitle_ShouldThrowDomainException()
+
+        public void Should_Throw_Exception_When_Content_Is_Null()
         {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                new string('A', 76),
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //new NewsCategory()
-            ));
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new News("Título de Teste", null));
+            Assert.Equal("O conteúdo da notícia não pode ser vazio.", exception.Message);
         }
 
         [Fact]
-        public void ValidateDomain_WithLongSubtitle_ShouldThrowDomainException()
+
+        public void Should_Throw_Exception_When_Content_Is_Empty()
         {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "Título da Notícia",
-                new string('A', 256),
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //new NewsCategory()
-            ));
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new News("Título de Teste", ""));
+            Assert.Equal("O conteúdo da notícia não pode ser vazio.", exception.Message);
         }
 
-        [Fact]
-        public void ValidateDomain_WithEmptyTitle_ShouldThrowDomainException()
-        {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "",
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //new NewsCategory()
-            ));
-        }
-
-        [Fact]
-        public void ValidateDomain_WithLongNewsBody_ShouldThrowDomainException()
-        {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "Título da Notícia",
-                "Subtítulo da Notícia",
-                new string('A', 65536),
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //new NewsCategory()
-            ));
-        }
-
-        [Fact]
-        public void ValidateDomain_WithLongImageUrl_ShouldThrowDomainException()
-        {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "Título da Notícia",
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                new string('A', 556),
-                DateTime.Now
-                //new NewsCategory()
-            ));
-        }
-
-        [Fact]
-        public void ValidateDomain_WithNullCategory_ShouldThrowDomainException()
-        {
-            var news = new News();
-            Assert.Throws<DomainExceptionValidation>(() => news.ValidateDomain(
-                NewsStatus.Publicada,
-                "https://example.com/news",
-                "Título da Notícia",
-                "Subtítulo da Notícia",
-                "Corpo da notícia.",
-                "https://example.com/image.jpg",
-                DateTime.Now
-                //null
-            ));
-        }
     }
 }
