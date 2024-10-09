52db63ed85ecba28ec714045c1731691f33824dc
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-10-08 23:06:59 -0300

    News DTO

---
+++ GwiNews/GwiNews.Application/DTOs/NewsDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GwiNews.Application.DTOs
+{
+    public class NewsDTO
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Status is required.")]
+        public NewsStatus Status { get; set; }
+
+        [MaxLength(255, ErrorMessage = "NewsUrl cannot exceed 255 characters.")]
+        public string NewsUrl { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(75, ErrorMessage = "Title cannot exceed 75 characters.")]
+        public string Title { get; set; }
+
+        [MaxLength(255, ErrorMessage = "Subtitle cannot exceed 255 characters.")]
+        public string Subtitle { get; set; }
+
+        [MaxLength(65535, ErrorMessage = "NewsBody cannot exceed 65,535 characters.")]
+        public string NewsBody { get; set; }
+
+        [MaxLength(555, ErrorMessage = "ImageUrl cannot exceed 555 characters.")]
+        public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "PublicationDate is required.")]
+        public DateTime PublicationDate { get; set; }
+
+        /*public UserWithNews Author { get; set; }
+
+        public UserWithNews Editor { get; set; }
+
+        public NewsCategory NewsCategory { get; set; }
+
+        public ICollection<NewsSubcategory> NewsSubcategories { get; set; }
+
+        public ICollection<ReaderUser> FavoritedByUsers { get; set; }*/
+    }
+}
+
