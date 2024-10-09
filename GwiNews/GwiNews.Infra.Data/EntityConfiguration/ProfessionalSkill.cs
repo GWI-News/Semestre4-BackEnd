6f920a34482dd31304d85a03d8b9204730acce36
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-09 12:47:27 -0300

    Implementar ProfessionalSkillConfiguration no EntityConfiguration

---
+++ GwiNews/GwiNews.Infra.Data/EntityConfiguration/ProfessionalSkill.cs
@@ -0,0 +1,37 @@
+﻿using GwiNews.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GwiNews.Infra.Data.EntityConfiguration
+{
+    public class ProfessionalSkillConfiguration : IEntityTypeConfiguration<ProfessionalSkill>
+    {
+        public void Configure(EntityTypeBuilder<ProfessionalSkill> builder)
+        {
+            // Chave primária
+            builder.HasKey(ps => ps.Id);
+
+            // Propriedade Id gerada automaticamente
+            builder.Property(ps => ps.Id)
+                   .IsRequired()
+                   .ValueGeneratedOnAdd();
+
+            // Propriedade Skill1 (habilidade 1)
+            builder.Property(ps => ps.Skill1)
+                   .IsRequired()
+                   .HasMaxLength(55);
+
+            // Propriedade Skill2 (habilidade 2)
+            builder.Property(ps => ps.Skill2)
+                   .HasMaxLength(55);
+
+            // Propriedade Skill3 (habilidade 3)
+            builder.Property(ps => ps.Skill3)
+                   .HasMaxLength(55);
+
+            // Propriedade Skill4 (habilidade 4)
+            builder.Property(ps => ps.Skill4)
+                   .HasMaxLength(55);
+        }
+    }
+}
