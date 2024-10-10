97b0b2e09521173790661b66bfcf20c59ccc6dbc
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-10-10 13:47:59 -0300

    Implementar a Interface INewsCategoryService no Diretório Interfaces e arrumar codigo comentado que depededia de category

---
+++ GwiNews/GwiNews.Application/Interfaces/INewsCategoryService.cs
@@ -0,0 +1,27 @@
+﻿using GwiNews.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GwiNews.Application.Interfaces
+{
+
+    public interface INewsCategoryService
+    {
+        Task<IEnumerable<NewsCategoryDTO>> GetNewsCategories();
+
+        Task<NewsCategoryDTO> GetNewsCategoryById(Guid id);
+
+        Task AddNewsCategory(NewsCategoryDTO categoryDto);
+
+        Task UpdateNewsCategory(NewsCategoryDTO categoryDto);
+
+        Task RemoveNewsCategory(Guid id);
+
+        Task<IEnumerable<NewsDTO>> GetNewsByCategory(Guid categoryId);
+
+        //Task<IEnumerable<NewsSubcategoryDTO>> GetNewsSubcategoriesByCategory(Guid categoryId);
+    }
+}
