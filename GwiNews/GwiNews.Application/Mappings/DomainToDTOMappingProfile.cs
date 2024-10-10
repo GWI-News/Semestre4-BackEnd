8d63899c49004bb1a5fc5574989447accf40e307
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-10-10 01:34:39 -0300

    Criação do Mapper de DTO para Entity.

---
+++ GwiNews/GwiNews.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GwiNews.Application.DTOs;
+using GwiNews.Domain.Entities;
+
+namespace GwiNews.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
