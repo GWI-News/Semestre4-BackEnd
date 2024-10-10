fafa82b65345e76a13da55528bd2ec3e4fe0d7b7
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-10-10 10:33:56 -0300

    Criação dos UserCommandHandler.

---
+++ GwiNews/GwiNews.Application/Users/Handlers/UserCreateCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using GwiNews.Application.Users.Commands;
+using GwiNews.Domain.Entities;
+using GwiNews.Infra.Data.Repositories;
+using MediatR;
+
+namespace GwiNews.Application.Users.Handlers
+{
+    public class UserCreateCommandHandler : IRequestHandler<UserCreateCommand, User>
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserCreateCommandHandler(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Handle(UserCreateCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User(request.Role, request.CompleteName, request.Email, request.Password, request.Status);
+
+            if (user == null)
+            {
+                throw new ApplicationException($"Error creating entity.");
+            }
+            else
+            {
+                return await _userRepository.CreateUserAsync(user);
+            }
+        }
+    }
+}
