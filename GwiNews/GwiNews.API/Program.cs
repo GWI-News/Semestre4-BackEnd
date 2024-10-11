f863bb57cf295bf0d02e515a52528814555cf9cf
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-10-10 22:27:33 -0300

    Testes e erros no migration

--- GwiNews/GwiNews.API/Program.cs
+++ GwiNews/GwiNews.API/Program.cs
@@ -1,25 +1,79 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using GwiNews.Application.Interfaces;
+using GwiNews.Application.Services;
+using GwiNews.Domain.Interfaces;
+using GwiNews.Infra.Data.Repositories;
+using GwiNews.Infra.IoC;
+using Serilog;
+using System.Text.Json;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+        try
+        {
+            Log.Information("Starting up");
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog();
+
+            builder.Services.AddInfrastructure(builder.Configuration);
+            builder.Services.AddInfrastructureJWT(builder.Configuration);
+            builder.Services.AddInfrastructureSwagger();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "AllowAll", builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseSerilogRequestLogging(); // Add Serilog request logging
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application start-up failed");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ GwiNews/GwiNews.Infra.IoC/DependencyInjectionSwagger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace GwiNews.Infra.IoC
+{
+    public static class DependencyInjectionSwagger
+    {
+        public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Por favor insira um JWT com Bearer no campo"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
