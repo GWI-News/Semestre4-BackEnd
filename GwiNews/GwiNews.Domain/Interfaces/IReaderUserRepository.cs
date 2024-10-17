33f0196ea81e92bccbd1c7eac0f53fd2ce322132
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-10-17 15:43:33 -0300

    Implementação da interface IReaderUserRepository e correção de bugs.

---
+++ GwiNews/GwiNews.Domain/Interfaces/IReaderUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using GwiNews.Domain.Entities;
+
+namespace GwiNews.Domain.Interfaces
+{
+    public interface IReaderUserRepository : IUserRepository
+    {
+        Task<IEnumerable<News>>? GetFavoritedNewsAsync(Guid? readerUserId);
+        Task AddFavoritedNewsAsync(Guid? readerUserId, News? news);
+        Task RemoveFavoritedNewsAsync(Guid? readerUserId, News? news);
+        Task<IEnumerable<ProfessionalInformation>>? GetProfessionalInformationsAsync(Guid? readerUserId);
+        Task AddProfessionalInformationAsync(Guid? readerUserId, ProfessionalInformation? professionalInformation);
+        Task RemoveProfessionalInformationAsync(Guid? readerUserId, ProfessionalInformation? professionalInformation);
+        Task<IEnumerable<Formation>>? GetFormationsAsync(Guid? readerUserId);
+        Task AddFormationAsync(Guid? readerUserId, Formation? formation);
+        Task RemoveFormationAsync(Guid? readerUserId, Formation? formation);
+        Task<IEnumerable<ProfessionalSkill>>? GetProfessionalSkillsAsync(Guid? readerUserId);
+        Task AddProfessionalSkillAsync(Guid? readerUserId, ProfessionalSkill? professionalSkill);
+        Task RemoveProfessionalSkillAsync(Guid? readerUserId, ProfessionalSkill? professionalSkill);
+    }
+}
--- GwiNews/GwiNews.Domain/Interfaces/IUserWithNewsRepository.cs
+++ GwiNews/GwiNews.Domain/Interfaces/IUserWithNewsRepository.cs
@@ -2,15 +2,10 @@
 
 namespace GwiNews.Domain.Interfaces
 {
-    public interface IUserWithNewsRepository
+    public interface IUserWithNewsRepository : IUserRepository
     {
-        Task<IEnumerable<UserWithNews>>? GetUsersAsync();
-        Task<UserWithNews>? GetByIdAsync(Guid? id);
-        Task<UserWithNews>? CreateAsync(UserWithNews? userWithNews);
-        Task<UserWithNews>? UpdateAsync(UserWithNews? userWithNews);
-        Task<UserWithNews>? RemoveAsync(UserWithNews? userWithNews);
-        Task<IEnumerable<UserWithNews>>? GetFilteredAsync(string? completeName);
-        Task<bool>? GetStatusAsync(Guid? userId);
-        Task UpdateStatusAsync(Guid? userId, bool? newStatus);
+        Task<IEnumerable<News>>? GetOwnNewsAsync(Guid? userWithNewsId);
+        Task AddOwnNewsAsync(Guid? userWithNewsId, News? news);
+        Task RemoveOwnNewsAsync(Guid? userWithNewsId, News? news);
     }
 }
