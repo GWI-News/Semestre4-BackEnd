03e0a9a0a52495be21450a4f5c1ffc4e19ea79cb
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-11-06 09:39:02 -0300

    Criação de Interface IUserWithNewsService e refatoração de Interface INewsService.

---
+++ GwiNews/GwiNews.Application/Interfaces/IUserWithNewsService.cs
@@ -0,0 +1,11 @@
+﻿using GwiNews.Application.DTOs;
+
+namespace GwiNews.Application.Interfaces
+{
+    public interface IUserWithNewsService : IUserService
+    {
+        Task<IEnumerable<NewsDTO>>? GetOwnNewsAsync(Guid? userWithNewsId);
+        Task AddOwnNewsAsync(Guid? userWithNewsId, NewsDTO? newsDto);
+        Task RemoveOwnNewsAsync(Guid? userWithNewsId, NewsDTO? newsDto);
+    }
+}
