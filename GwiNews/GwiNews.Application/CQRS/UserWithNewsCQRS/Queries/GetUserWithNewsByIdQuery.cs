79d255168400ec19adde4e1f263f23788155a57e
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-11-13 00:18:49 -0300

    Criação das classes Queries para UserWwithNews.

---
+++ GwiNews/GwiNews.Application/CQRS/UserWithNewsCQRS/Queries/GetUserWithNewsByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using GwiNews.Application.CQRS.UsersCQRS.Queries;
+using GwiNews.Domain.Entities;
+using MediatR;
+
+namespace GwiNews.Application.CQRS.UserWithNewsCQRS.Queries
+{
+    public class GetUserWithNewsByIdQuery : GetUserByIdQuery, IRequest<UserWithNews>
+    {
+        public GetUserWithNewsByIdQuery(Guid? id)
+            : base(id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ GwiNews/GwiNews.Application/CQRS/UserWithNewsCQRS/Queries/GetUsersWithNewsQuery.cs
@@ -0,0 +1,10 @@
+﻿using GwiNews.Application.CQRS.UsersCQRS.Queries;
+using GwiNews.Domain.Entities;
+using MediatR;
+
+namespace GwiNews.Application.CQRS.UserWithNewsCQRS.Queries
+{
+    public class GetUserWithNewsQuery : GetUsersQuery, IRequest<IEnumerable<UserWithNews>>
+    {
+    }
+}
