f245ee4f1636ef03b2cd5d15ea8ba5d99c6915f9
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-11-12 23:05:01 -0300

    Implementar a Classe ReaderUserDTO no Diretório Application.DTOs

---
+++ GwiNews/GwiNews.Application/DTOs/ReaderUserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GwiNews.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Application.Enums;
+
+
+namespace GwiNews.Application.DTOs
+{
+    public class ReaderUserDTO : UserDTO
+    {
+        [DisplayName("Notícias Favoritadas")]
+        public ICollection<NewsDTO> FavoritedNews { get; set; }
+
+        [DisplayName("Informações Profissionais")]
+        public ICollection<ProfessionalInformationDTO> ProfessionalInformations { get; set; }
+
+        [DisplayName("Habilidades Profissionais")]
+        public ICollection<ProfessionalSkillDTO> ProfessionalSkills { get; set; }
+
+        [DisplayName("Formações Acadêmicas")]
+        public ICollection<FormationDTO> Formations { get; set; }
+    }
+}
