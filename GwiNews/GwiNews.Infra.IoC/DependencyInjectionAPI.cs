35f5402fc99af4d6ff2ed1f4abe82c8ac8d629c8
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-11-26 19:21:29 -0300

    Refatoração da classe DependencyInjectionAPI e adição das configurações de UserWithNews à esta classe.

--- GwiNews/GwiNews.Infra.IoC/DependencyInjectionAPI.cs
+++ GwiNews/GwiNews.Infra.IoC/DependencyInjectionAPI.cs
@@ -1,44 +1,55 @@
-﻿//using GwiNews.Application.Categories.Handlers;
-//using GwiNews.Application.Interfaces;
-//using GwiNews.Application.Mappings;
-//using GwiNews.Application.Services;
-//using GwiNews.Application.Users.Handlers;
-//using GwiNews.Domain.Interfaces;
-//using GwiNews.Infra.Data.Context;
-//using GwiNews.Infra.Data.Repositories;
-//using Infra.Data.Repositories;
-//using Interfaces;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using GwiNews.Application.Interfaces;
+using GwiNews.Application.Mappings;
+using GwiNews.Application.Services;
+using GwiNews.Domain.Interfaces;
+using GwiNews.Infra.Data.Context;
+using GwiNews.Infra.Data.Repositories;
+using GwiNews.Application.CQRS.UsersCQRS.Handlers;
+using GwiNews.Application.CQRS.CategoriesCQRS.Handlers;
+using GwiNews.Application.CQRS.NewsCQRS.Handlers;
+using GwiNews.Application.CQRS.UserWithNewsCQRS.Handlers;
 
-//namespace GwiNews.Infra.IoC
-//{
-//    public static class DependencyInjectionAPI
-//    {
-//        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
-//            IConfiguration configuration)
-//        {
-//            services.AddDbContext<ApplicationDbContext>(options =>
-//                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
-//                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+namespace GwiNews.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
-//            services.AddScoped<IUserRepository, UserRepository>();
-//            services.AddScoped<INewsCategoryRepository, NewsCategoryRepository>();
+            services.AddScoped<IFormationRepository, FormationRepository>();
+            services.AddScoped<INewsCategoryRepository, NewsCategoryRepository>();
+            services.AddScoped<INewsRepository, NewsRepository>();
+            services.AddScoped<INewsSubcategoryRepository, NewsSubcategoryRepository>();
+            services.AddScoped<IProfessionalInformationRepository, ProfessionalInformationRepository>();
+            services.AddScoped<IProfessionalSkillRepository, ProfessionalSkillRepository>();
+            services.AddScoped<IReaderUserRepository, ReaderUserRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserWithNewsRepository, UserWithNewsRepository>();
 
-//            services.AddScoped<IUserService, UserService>();
-//            services.AddScoped<INewsCategoryService, NewsCategoryService>();
+            services.AddScoped<INewsCategoryService, NewsCategoryService>();
+            services.AddScoped<INewsService, NewsService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserWithNewsService, UserWithNewsService>();
 
-//            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
-//            services.AddMediatR(cfg =>
-//            {
-//                cfg.RegisterServicesFromAssembly(typeof(UserCreateQueryHandler).Assembly);
+            var myHandlers = AppDomain.CurrentDomain.Load("GwiNews.Application");
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(UserCreateCommandHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(NewsCategoryCreateCommandHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(NewsCreateCommandHandler).Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(UserWithNewsCreateCommandHandler).Assembly);
+            });
 
-//                cfg.RegisterServicesFromAssembly(typeof(NewsCategoryCreateCommandHandler).Assembly);
-//            });
-
-//            return services;
-//        }
-//    }
-//}
+            return services;
+        }
+    }
+}
