bb9eb8ea9736e60cac161d39205c6473dfc7b83f
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-11-27 10:19:36 -0300

    correção 2

--- GwiNews/GwiNews.Application/Interfaces/INewsSubcategoryService.cs
+++ GwiNews/GwiNews.Application/Interfaces/INewsSubcategoryService.cs
@@ -1,15 +1,16 @@
 ﻿using GwiNews.Application.DTOs;
+using GwiNews.Domain.Entities;
 
 namespace GwiNews.Application.Interfaces
 {
     public interface INewsSubcategoryService
     {
-        Task<IEnumerable<NewsSubcategory>> GetSubcategoriesAsync();
-        Task<NewsSubcategory> GetByIdAsync(Guid id);
-        Task<NewsSubcategory> CreateAsync(NewsSubcategory subcategory);
-        Task<NewsSubcategory> UpdateAsync(NewsSubcategory subcategory);
-        Task<NewsSubcategory> RemoveAsync(NewsSubcategory subcategory);
-        Task<IEnumerable<NewsSubcategory>> GetFilteredAsync(string name);
+        Task<IEnumerable<NewsSubcategoryDTO>> GetSubcategoriesAsync();
+        Task<NewsSubcategoryDTO> GetByIdAsync(Guid id);
+        Task<NewsSubcategoryDTO> CreateAsync(NewsSubcategory subcategory);
+        Task<NewsSubcategoryDTO> UpdateAsync(NewsSubcategory subcategory);
+        Task<NewsSubcategoryDTO> RemoveAsync(NewsSubcategory subcategory);
+        Task<IEnumerable<NewsSubcategoryDTO>> GetFilteredAsync(string name);
 
     }
 }
