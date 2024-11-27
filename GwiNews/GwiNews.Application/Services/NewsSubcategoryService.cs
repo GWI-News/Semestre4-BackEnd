068147b9b9505b2e32334e376b070a3148abdb3b
SoTavares2 <sophia.tavares@fatec.sp.gov.br>
2024-11-27 09:36:22 -0300

    Implementar o Serviço NewsSubcategoryService no Diretório Services

---
+++ GwiNews/GwiNews.Application/Services/NewsSubcategoryService.cs
@@ -0,0 +1,61 @@
+﻿using GwiNews.Application.Interfaces;
+using GwiNews.Application.DTOs;
+using GwiNews.Domain.Entities;
+using GwiNews.Domain.Interfaces;
+
+namespace GwiNews.Application.Services
+{
+        public class NewsSubcategoryService : INewsSubcategoryService
+        {
+            private readonly INewsSubcategoryRepository _newsSubcategoryRepository;
+            private readonly IMapper _mapper;
+
+            public NewsSubcategoryService(INewsSubcategoryRepository newsSubcategoryRepository, IMapper mapper)
+            {
+                _newsSubcategoryRepository = newsSubcategoryRepository;
+                _mapper = mapper;
+            }
+
+            public async Task AddNewsSubcategory(NewsSubcategoryDTO subcategoryDto)
+            {
+                var subcategoryEntity = _mapper.Map<NewsSubcategory>(subcategoryDto);
+                await _newsSubcategoryRepository.AddNewsSubcategory(subcategoryEntity);
+            }
+
+            public async Task<IEnumerable<NewsSubcategoryDTO>> GetNewsSubcategories()
+            {
+                var subcategoryEntities = await _newsSubcategoryRepository.GetAllNewsSubcategories();
+                return _mapper.Map<IEnumerable<NewsSubcategoryDTO>>(subcategoryEntities);
+            }
+
+            public async Task<NewsSubcategoryDTO> GetNewsSubcategoryById(Guid id)
+            {
+                var subcategoryEntity = await _newsSubcategoryRepository.GetNewsSubcategoryById(id);
+                return _mapper.Map<NewsSubcategoryDTO>(subcategoryEntity);
+            }
+
+            public async Task RemoveNewsSubcategory(Guid id)
+            {
+                var subcategoryEntity = await _newsSubcategoryRepository.GetNewsSubcategoryById(id);
+                await _newsSubcategoryRepository.DeleteNewsSubcategory(subcategoryEntity.Id);
+            }
+
+            public async Task UpdateNewsSubcategory(NewsSubcategoryDTO subcategoryDto)
+            {
+                var subcategoryEntity = _mapper.Map<NewsSubcategory>(subcategoryDto);
+                await _newsSubcategoryRepository.UpdateNewsSubcategory(subcategoryEntity);
+            }
+
+            public async Task<IEnumerable<NewsDTO>> GetNewsBySubcategory(Guid subcategoryId)
+            {
+                var newsEntities = await _newsSubcategoryRepository.GetNewsBySubcategory(subcategoryId);
+                return _mapper.Map<IEnumerable<NewsDTO>>(newsEntities);
+            }
+
+            public async Task<NewsCategoryDTO> GetNewsCategoryBySubcategory(Guid subcategoryId)
+            {
+                var categoryEntity = await _newsSubcategoryRepository.GetNewsCategoryBySubcategory(subcategoryId);
+                return _mapper.Map<NewsCategoryDTO>(categoryEntity);
+            }
+        }
+}
