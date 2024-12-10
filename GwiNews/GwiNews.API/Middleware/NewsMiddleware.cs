964d7f17d9d30b8b330d7dbbc05d9bea267a950f
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-12-10 16:20:01 -0300

    Implementação das classe de Middleware para NewsController.

---
+++ GwiNews/GwiNews.API/Middleware/NewsMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace GwiNews.API.Middleware
+{
+    public class NewsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<NewsMiddleware> _logger;
+
+        public NewsMiddleware(RequestDelegate next, ILogger<NewsMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                // Log de entrada da requisição para rotas relacionadas a "api/news"
+                if (context.Request.Path.StartsWithSegments("/api/news"))
+                {
+                    _logger.LogInformation($"Processing request: {context.Request.Method} {context.Request.Path}");
+                }
+
+                await _next(context);
+
+                // Log de saída bem-sucedida para rotas relacionadas a "api/news"
+                if (context.Request.Path.StartsWithSegments("/api/news"))
+                {
+                    _logger.LogInformation($"Response Status Code: {context.Response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error processing request: {context.Request.Method} {context.Request.Path}");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(new { error = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ GwiNews/GwiNews.API/Middleware/NewsMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace GwiNews.API.Middleware
+{
+    public static class NewsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseNewsMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<NewsMiddleware>();
+        }
+    }
+}
